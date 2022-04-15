fc6d0890b2730c39c98e290c81e3dc60d98b4d23
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2022-04-15 10:11:12 +0800

    update

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -2,26 +2,36 @@ import './App.css';
 import React from 'react';
 //Components
 import UserInput from './components/UserInput';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-	const [name, setName] = React.useState({ name: '' });
+	const [name, setName] = React.useState({ id: '', name: '' });
 
 	const [friendsArray, setFriendsArray] = React.useState([]);
 
-	function handleChange(event) {
-		const { name, value } = event.target;
-
+	function handleChange(name) {
 		setName((nameState) => {
 			return {
 				...nameState,
-				[name]: value,
+				id: uuidv4(),
+				name: name,
 			};
 		});
+
+		if (name.name === '') {
+			console.log('Please enter a name');
+		}
 	}
 
 	const handleClick = () => {
-		setFriendsArray([name, ...friendsArray]);
+		setName((nameState) => {
+			return {
+				...nameState,
+				id: uuidv4(),
+			};
+		});
 
+		setFriendsArray([name, ...friendsArray]);
 		console.log(friendsArray);
 	};
 
@@ -31,6 +41,7 @@ function App() {
 				handleClick={handleClick}
 				name={name}
 				handleChange={handleChange}
+				friendsArray={friendsArray}
 			/>
 		</div>
 	);
--- react-practice/src/components/UserInput.jsx
+++ react-practice/src/components/UserInput.jsx
@@ -1,20 +1,24 @@
-function UserInput({ handleClick, handleChange, name }) {
+function UserInput({ handleClick, handleChange, name, friendsArray }) {
 	return (
 		<div className="user-input">
 			<input
 				type="text"
 				placeholder="Enter your name"
-				onChange={handleChange}
+				onChange={(e) => handleChange(e.target.value)}
 				name="name"
-				value={name.name}
+				required
 			/>
 			<button onClick={handleClick}>Add</button>
 
 			<h1>List of friends</h1>
 			<div className="friends-holder">
-				<div className="friends-lists">
-					<p></p>
-				</div>
+				{friendsArray.map((item) => {
+					return (
+						<div className="friends-lists" key={item.id}>
+							<p>{item.name}</p>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
